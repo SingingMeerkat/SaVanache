2ee1c75299cf6f5d637d97b9dfb5af8a56699d25
Laure LEBON <laure.lebon@web-atrio.com>
2021-11-02 11:46:24 +0100

    display of multiple charts

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div>
-    <h1>Try to display a chart with d3.js</h1>
-    <Chart />
+    <h1>Try to display a chart with d3.js</h1> 
+        <ul class="wrapper-grid">
+          <li v-for="(chromosome, index) in chromosomes" :key="index">
+            <Chart :chromosome="chromosome" />
+          </li>
+        </ul>
   </div>
 </template>
 
 <script>
 import Chart from "./components/Chart.vue";
-
+import chromosomes from "./data.json"
 export default {
   name: "App",
+  data() {
+    return {
+      chromosomes : chromosomes
+    }
+  },
   components: {
     Chart: Chart,
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style lang="scss" scoped>
+h1 {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+li {
+  list-style: none;
 }
 </style>
--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -7,47 +7,60 @@ import * as d3 from "d3";
 
 export default {
   name: "Chart",
+  props: {
+    chromosome: {
+      type: Object
+    }
+  },
+  // created () {
+  //     Object.entries(this.chromosomes).forEach(
+  //       ([key]) => {
+  //         let data = this.chromosomes[key]
+  //         let data2 = { name : key};
+  //         for (let i = 0; i < data.length; i++) {
+  //               data[i] = Object.assign(data[i], data2);
+  //         }
+          
+  //         this.chromosomeList.push(data)
+  //       }
+  
+  //   );
+  // },
   mounted() {
-    // set the dimensions and margins of the graph
-      var margin = {top: 10, right: 30, bottom: 30, left: 60},
-          width = 1000 - margin.left - margin.right,
-          height = 400 - margin.top - margin.bottom;
+          // set the dimensions and margins of the graph
+      var margin = {top: 30, right: 0, bottom: 30, left: 50},
+          width = 210 - margin.left - margin.right,
+          height = 210 - margin.top - margin.bottom;
 
-      // append the svg object to the body of the page
-      var svg = d3.select("#chart-vue")
+          //Read the data
+            let data = this.chromosome
+
+
+
+        // Add an svg element for each group. The will be one beside each other and will go on the next row when no more room available
+              var svg = d3.select("#chart-vue")
+
         .append("svg")
           .attr("width", width + margin.left + margin.right)
           .attr("height", height + margin.top + margin.bottom)
         .append("g")
           .attr("transform",
                 "translate(" + margin.left + "," + margin.top + ")");
+        
 
-                  //Read the data
-      d3.csv("/variability_forChrom1.csv",
 
-              // When reading the csv, I must format variables:
-              function(d){
-                return { position : d.position, varIndex : d.varIndex }
-              }).then(
-
-              // Now I can use this dataset:
-                function(data) {
-                  console.log(data)
-                    // Add X axis --> it is a date format
-                  var x = d3.scaleLinear()
+       var x = d3.scaleLinear()
                     .domain([0, 250000000])
                     .range([ 0, width ]);
                   svg.append("g")
                     .attr("transform", "translate(0," + height + ")")
-                    .call(d3.axisBottom(x));
 
                   // Add Y axis
                   var y = d3.scaleLinear()
-                    .domain([0, 45000])
+                    .domain([0, 85000])
                     .range([ height, 0 ]);
                   svg.append("g")
-                    .call(d3.axisLeft(y));
-
+ 
                     // Add the line
                     svg.append("path")
                       .datum(data)
@@ -59,12 +72,13 @@ export default {
                         .y(function(d) { return y(d.varIndex) })
                         )
 
-            })
+
 
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
+
 </style>
