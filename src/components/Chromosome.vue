a44f1bd22dcfc2e5f6c312a55b5b635d69b42e88
Laure LEBON <laure.lebon@web-atrio.com>
2021-11-08 15:13:43 +0100

    Start and close charts with lines and filled with polygon

--- src/components/Chromosome.vue
+++ src/components/Chromosome.vue
@@ -11,14 +11,17 @@
                 <stop v-for="(color, i) in colorRange" :key="i" :offset="`${++i*(100/colorRange.length)}%`" :stop-color="color"></stop>
               </linearGradient>
           </defs>
-          <g :transform="transform" fill="url(#lingrad)">
+          
+          <g :transform="transform" fill="url(#lingrad)" class="group">
+            <line :x1="x1Line1" :y1="y1Line1" :x2="x2Line1" :y2="y2Line1" style="stroke:rgb(0,0,0);stroke-width:3" />
+            <line :x1="x1Line2" :y1="y1Line2" :x2="x2Line2" :y2="y2Line2" style="stroke:rgb(0,0,0);stroke-width:3" />
             <path
               stroke="#9579a6"
               stroke-width= "2"
               :d="line"
             >
             </path>
-            <rect :x="xRect" :y="yRect" :width="widthRect" :height="heightRect" />
+            <polygon :points="`${x1Line1},${y1Line1} ${x2Line1},${y2Line1} ${x2Line2},${y2Line2} ${x1Line2},${y1Line2}`"/>
             <path
               stroke="#9579a6"
               stroke-width= "2"
@@ -46,12 +49,24 @@ export default {
       height: 480,
       width: 1000,
       transform: "translate(25,15)",
-      xRect: "",
-      yRect: "",
-      widthRect: "",
-      heightRect: "",
+      x1Line1: "",
+      y1Line1: "",
+      x2Line1: "",
+      y2Line1: "",
+      x1Line2: "",
+      y1Line2: "",
+      x2Line2: "",
+      y2Line2: "",
       colorRange: ["#351D0F", "#602C17", "#720825", "#840A77", "#8332CF", "#5F63E5", "#5687E2", "#3B97C6", "#32A69D", "#18BD78", "#3CD644", "#A2E04D", "#CDD41C", "#DDD435", "#FCDF91"]
     };
+  },
+  methods: {
+    getLength(arr) {
+      return arr.length
+    },
+    getMaxWidth(arr) {
+      return d3.max(arr, d => d.position )
+    }
   },
   computed: {
     path() {
@@ -63,22 +78,23 @@ export default {
                   .domain([-10000, 85000])
                   .range([ this.height - this.marginTop - this.marginBottom, 0 ])
 
-     const maxX = d3.max(this.chromosome, d => d.position )
-
       return d3.line()
         .x((d, i) => {
-          console.log(d.position)
-            if(i===0) {
-              this.xRect = x(d.position)
-              this.widthRect = x(maxX-i)
+            if(i === 0) {
+              this.x1Line1 = x(d.position) 
+            }
+            if(i === this.getLength(this.chromosome)-1) {
+              this.x1Line2 = x(d.position)
             }
             return x(d.position)
         } )
         .y((d, i) => {
             if(i===0) {
-                this.yRect = y(d.position)
+                this.y1Line1 = y(d.varIndex)
             }
-            this.heightRect = "12"
+            if(i === this.getLength(this.chromosome)-1) {
+              this.y1Line2 = y((d.varIndex))
+            }
             return y(d.varIndex)
         })
     },
@@ -90,10 +106,26 @@ export default {
       const y = d3.scaleLinear()
                   .domain([0, 85000])
                   .range([ this.height - this.marginTop - this.marginBottom, 0 ])
-
+      
       return d3.line()
-        .x(d => x(d.position))
-        .y(d => y((d.varIndex*-1)));
+        .x((d, i) => {
+            if(i===0) {
+              this.x2Line1 = x(d.position)
+            }
+            if(i === this.getLength(this.chromosome)-1) {
+              this.x2Line2 = x(d.position)
+            }
+            return x(d.position)
+        } )
+        .y((d, i) => {
+            if(i===0) {
+                this.y2Line1 = y((d.varIndex*-1))
+            }
+            if(i === this.getLength(this.chromosome)-1) {
+              this.y2Line2 = y((d.varIndex*-1))
+            }
+            return y((d.varIndex*-1))
+        })
     },
     line() {
       return this.path(this.chromosome)
