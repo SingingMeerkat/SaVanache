801a0803df1dd4a5ead6acd408ff36527b08b01a
Laure LEBON <laure.lebon@web-atrio.com>
2021-12-21 18:09:00 +0100

    Bug fixed

--- src/App.vue
+++ src/App.vue
@@ -288,8 +288,8 @@ body {
             padding-bottom: 2rem;
             width: 95%;
             display: grid;
-            grid-template-columns: repeat(12, 1fr);
-            grid-template-rows: 1fr 1fr;
+            grid-template-columns: repeat(24, 1fr);
+            grid-template-rows: 1fr;
             overflow-x: scroll;
 
             &::-webkit-scrollbar {
